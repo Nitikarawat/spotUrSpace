a0504cf9c9c0558838e4214657f2d7263889a6b7
NIKIRaw <73019582+Nitikarawat@users.noreply.github.com>
2025-03-13 23:22:00 +0530

    satic Room Cards Done

--- Components/CardRoom.jsx
+++ Components/CardRoom.jsx
@@ -6,15 +6,15 @@ const CardRoom = ({ room }) => {
         <div
         className="bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center"
       >
-        <div className="flex flex-col sm:flex-row sm:space-x-4">
+        <div className="flex flex-col sm:flex-row sm:space-x-4 ">
           <Image
             src={`/images/Rooms/${ room.image }`}
             width={400}
             height={100}
             alt={room.name}
-            className="w-full sm:w-32 sm:h-32 mb-3 sm:mb-0 object-cover rounded-lg"
+            className="w-full sm:w-50 sm:h-50 mb-3 sm:mb-0 object-cover rounded-lg"
           />
-          <div className="space-y-1">
+          <div className="space-y-1 mt-3">
             <h4 className="text-lg font-semibold">{ room.name }</h4>
             <p className="text-sm text-gray-600">
               <span className="font-semibold text-gray-800"> Address: </span>
@@ -31,11 +31,11 @@ const CardRoom = ({ room }) => {
           </div>
         </div>
         <div
-          className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0"
-        >
+          class="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0"
+          >
           <Link
             href={`/rooms/${room.$id}`}
-            className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
+            class="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
             >View Room
             </Link>
         </div>
--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,14 @@ import rooms from "@/data/rooms.json"
 import CardRoom from "@/Components/CardRoom";
 export default function Home() {
   return (
-    <>
-    {/* room prop is current room */}
+    <div >
+   {/* room prop is current room */}
     {rooms.length > 0 ? (
       rooms.map((room) => <CardRoom room={room}/>)
     )
 
     
     : ( <p>No Rooms Available Right Now</p>) }
-    </>
+    </div>
   );
 }
