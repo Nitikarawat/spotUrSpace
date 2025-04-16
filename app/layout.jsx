2c627a6fc0aadcc49d0f151cb6c14ea50cbc433d
NIKIRaw <73019582+Nitikarawat@users.noreply.github.com>
2025-04-17 02:43:53 +0530

    End Submission 17042025(4)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import Header from "@/Components/Header";
+import Header from "./Components/Header";
 import Footer from "@/Components/Footer";
 import { ToastContainer } from "react-toastify";
 import AuthWrapper from "@/Components/AuthWrapper";
